d0fe1d20e57099f724d35a363b712d2049d33459
Ghouli <huhtala.teemu@gmail.com>
2018-02-13 12:04:04 +0200

    Fix test for url title

--- test/trumpet_test.exs
+++ test/trumpet_test.exs
@@ -108,8 +108,8 @@ defmodule TrumpetTest do
   end
 
   test "title fetching" do
-    assert Trumpet.Commands.fetch_title("https://imgur.com/r/space/wQTN1Cj") == "Albert II, the first monkey in space - Imgur"
-    assert Trumpet.Commands.fetch_title("https://i.imgur.com/wQTN1Cj.jpg") == "Albert II, the first monkey in space - Imgur"
+    assert Trumpet.Website.fetch_title("https://imgur.com/r/space/wQTN1Cj") == "Albert II, the first monkey in space - Imgur"
+    assert Trumpet.Website.fetch_title("https://i.imgur.com/wQTN1Cj.jpg") == "Albert II, the first monkey in space - Imgur"
   end
 
   test "lotto generator" do
